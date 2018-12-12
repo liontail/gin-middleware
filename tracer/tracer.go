59f2c3731a9fa83d396664bd5192670b60374e41
ZaiEf <zaief2@gmail.com>
2018-12-12 13:49:18 +0700

    add tag json and bson

--- tracer/tracer.go
+++ tracer/tracer.go
@@ -1,6 +1,7 @@
 package tracer
 
 import (
+	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -9,8 +10,8 @@ import (
 
 // Tracer represent state for each request.
 type Tracer struct {
-	TraceID string
-	Now     time.Time
+	TraceID string    `json:"trace_id" bson:"trace_id"`
+	Now     time.Time `json:"requested_time" bson:"requested_time"`
 }
 
 func TracerMW() gin.HandlerFunc {
@@ -19,7 +20,7 @@ func TracerMW() gin.HandlerFunc {
 		// t := time.Now()
 
 		// GET traceID from header if there is none generate one with uuid
-		traceID := c.Request.Header.Get("X-Trace-ID")
+		traceID := c.Request.Header.Get("zr-request-id")
 		if traceID == "" {
 			traceID = uuid.New().String()
 		}
@@ -27,6 +28,7 @@ func TracerMW() gin.HandlerFunc {
 			TraceID: traceID,
 			Now:     time.Now(),
 		})
+
 		c.Next()
 		// // after request
 
@@ -36,5 +38,42 @@ func TracerMW() gin.HandlerFunc {
 		// // access the status we are sending
 		// status := c.Writer.Status()
 		// log.Println(status)
+	}
+}
+
+type Options struct {
+	Prefix              string
+	LatencyEnable       bool
+	SendingStatusEnable bool
+}
+
+func TracerMwWithOptions(options Options, traceHeader string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// before request
+		t := time.Now()
+
+		// GET traceID from header if there is none generate one with uuid
+		traceID := c.Request.Header.Get(traceHeader)
+		if traceID == "" {
+			traceID = options.Prefix + "_" + uuid.New().String()
+		}
+		c.Set("Tracer", Tracer{
+			TraceID: traceID,
+			Now:     time.Now(),
+		})
+		c.Next()
+
+		// after request
+
+		if options.LatencyEnable {
+			latency := time.Since(t)
+			log.Println(latency)
+		}
+
+		// access the status we are sending
+		if options.SendingStatusEnable {
+			status := c.Writer.Status()
+			log.Println(status)
+		}
 	}
 }
