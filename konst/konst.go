3a01ccf4d704881f074a19325b3490e6791892c6
ZaiEf <zaief2@gmail.com>
2019-02-05 13:49:02 +0700

    add Logger Default and Option to enable tracer object

---
+++ konst/konst.go
@@ -0,0 +1,13 @@
+package konst
+
+var (
+	GreenColor   = string([]byte{27, 91, 57, 55, 59, 52, 50, 109})
+	WhiteColor   = string([]byte{27, 91, 57, 48, 59, 52, 55, 109})
+	YellowColor  = string([]byte{27, 91, 57, 48, 59, 52, 51, 109})
+	RedColor     = string([]byte{27, 91, 57, 55, 59, 52, 49, 109})
+	BlueColor    = string([]byte{27, 91, 57, 55, 59, 52, 52, 109})
+	MagentaColor = string([]byte{27, 91, 57, 55, 59, 52, 53, 109})
+	CyanColor    = string([]byte{27, 91, 57, 55, 59, 52, 54, 109})
+	ResetColor   = string([]byte{27, 91, 48, 109})
+	DisableColor = false
+)
--- tracer/tracer.go
+++ tracer/tracer.go
@@ -1,11 +1,13 @@
 package tracer
 
 import (
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/liontail/gin-middleware/konst"
 )
 
 // Tracer represent state for each request.
@@ -45,30 +47,37 @@ type Options struct {
 	Prefix              string
 	LatencyEnable       bool
 	SendingStatusEnable bool
+	TracerObjectEnable  bool
 }
 
 func TracerMwWithOptions(options Options, traceHeader string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// before request
-		t := time.Now()
+		if options.LatencyEnable {
+			t := time.Now()
+			// Excute after end this middleware func
+			defer func() {
+				latency := time.Since(t)
+				log.Println(latency)
+			}()
+		}
 
 		// GET traceID from header if there is none generate one with uuid
 		traceID := c.Request.Header.Get(traceHeader)
 		if traceID == "" {
 			traceID = options.Prefix + "_" + uuid.New().String()
+			c.Request.Header.Set(traceHeader, traceID)
 		}
-		c.Set("Tracer", Tracer{
-			TraceID: traceID,
-			Now:     time.Now(),
-		})
+
+		if options.TracerObjectEnable {
+			c.Set("Tracer", Tracer{
+				TraceID: traceID,
+				Now:     time.Now(),
+			})
+		}
 		c.Next()
 
 		// after request
-
-		if options.LatencyEnable {
-			latency := time.Since(t)
-			log.Println(latency)
-		}
 
 		// access the status we are sending
 		if options.SendingStatusEnable {
@@ -76,4 +85,19 @@ func TracerMwWithOptions(options Options, traceHeader string) gin.HandlerFunc {
 			log.Println(status)
 		}
 	}
+}
+
+func LoggerDefault() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s %s %s] %s %-7s %s | %s %d %s | \"%s %s  %s %s\"\n",
+			param.TimeStamp.Format(time.RFC3339),
+			konst.RedColor, param.Request.Header.Get("zr-request-id"), konst.ResetColor,
+			konst.BlueColor, param.Method, konst.ResetColor,
+			konst.GreenColor, param.StatusCode, konst.ResetColor,
+			param.Path,
+			param.Request.Proto,
+			param.Latency,
+			param.ErrorMessage,
+		)
+	})
 }
